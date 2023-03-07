7ebbe891c4e29301d472bbcf8f0f82111f85fba6
pexApred <120075705+pexApred@users.noreply.github.com>
2023-03-06 20:06:41 -0500

    added beginnings of js, specific to fetching dictionary data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
 
 var addWords = function() {
     console.log("addWords");
+    var word = $("#new-word").val();
+    console.log(word);
 
+
+
+}
+
+var initListeners = function(){
+    console.log("init listeners");
     
 }
 
 $(function() {
     console.log("init");
+    initListeners();
     addWords();
 });
