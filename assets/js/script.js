32ba85f0bedb39100ba4c803be281fbc7a0025c6
pexApred <120075705+pexApred@users.noreply.github.com>
2023-03-07 21:19:28 -0500

    edited js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,8 @@ var addWords = function() {
     var word = $("#new-word").val();
     
     var wordUrl = "https://api.dictionaryapi.dev/api/v2/entries/en_US/" + word;
-    var randomWordUrl = "https://api.api-ninjas.com/v1/randomword";
 
-    fetch(randomWordUrl)
+    fetch(wordUrl)
         .then( function(response) {
             return response.json();
         })
@@ -14,6 +13,29 @@ var addWords = function() {
             console.log("fetched word", data);
         });
 
+}
+
+var addRandomWords = function() {
+    console.log("add random words")
+    var randomWord =$("#random-word-search-button");
+   
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/randomword',
+        headers: { 'X-Api-Key': "rbypcBezrmrt/yj6k+bxXg==JjPS9OzXlA7lAUl8"},
+        contentType: 'application/json',
+        success: function(result) {
+            console.log(result);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+
+}
+
+var addPage = function(){
+    console.log("add page");
 }
 
 var initListeners = function(){
@@ -26,10 +48,14 @@ var initListeners = function(){
     });
 
     $("#new-word-search-button").click(addWords);
+    $("#random-word-search-button").click(addRandomWords);
+    $("#page").click(addPage);
 }
-
+// 
 $(function() {
     console.log("init");
     initListeners();
     addWords();
+    addRandomWords();
+    addPage();
 });
