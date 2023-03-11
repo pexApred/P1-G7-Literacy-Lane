0c234325396895163dcbaf94e85b6056b5dd641a
pexApred <120075705+pexApred@users.noreply.github.com>
2023-03-11 14:16:07 -0500

    added spinner icon & reworked error handling to be more user friendly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,25 @@
 // Emmanuel
 // Dictionary API Fetch Function below
+var spinner = $(".giphy-embed");
+
 var addWords = function() {
     console.log("addWords");
-    var word = $("#new-word").val();
-    
+    $("#generated-word").hide();
+    spinner.show();
+
+    var word = $("#new-word").val() || "Literacy";
     var wordUrl = "https://api.dictionaryapi.dev/api/v2/entries/en_US/" + word;
 
     fetch(wordUrl)
         .then( function(response) {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
             return response.json();
         })
         .then( function(data){
             console.log("fetched word", data);
+            spinner.hide();
 
             var partOfSpeech = data[0].meanings[0].partOfSpeech;
             var definition = data[0].meanings[0].definitions[0].definition;
@@ -21,26 +29,35 @@ var addWords = function() {
             var phoneticAudio = data[0].phonetics.find(function(phonetics) {
                 return /mp3$/.test(phonetics.audio);
             }).audio;
-            console.log(definition);
-            console.log(phoneticAudio);
             
-            if(partOfSpeech && definition && phoneticText && phoneticAudio) {
+            if(word && partOfSpeech && definition && phoneticText && phoneticAudio) {
                 var audioEl = document.createElement("audio");
                 audioEl.src = phoneticAudio;
                 audioEl.controls = true;
 
                 $("#generated-word").html("<h2>" + word + "</h2><p>Phonetics: " + phoneticText + "</p><p>Part of Speech: " + partOfSpeech + "</p><p>Definition: " + definition + "<p>");
                 $("#generated-word").append(audioEl);
+
+                spinner.hide();
+                $("#generated-word").show();
             } else {
                 addRandomWords();
             }
+        })
+        .catch(function(error){
+            console.error('Error:', error);
+            spinner.hide();
+            $("#generated-word").html("<p>Information not available. Please try again.</p>");
+            $("#generated-word").show();
+        
         });
 
 }
 // Random Word generator API from API Ninjas
 var addRandomWords = function() {
     console.log("add random words")
-    // var randomWord =$("#random-word-search-button");
+
+    spinner.show();
    
     $.ajax({
         method: 'GET',
@@ -53,12 +70,19 @@ var addRandomWords = function() {
             var wordUrl = "https://api.dictionaryapi.dev/api/v2/entries/en_US/" + result.word;
             fetch(wordUrl)
                 .then(function(response){
+                    if (!response.ok) {
+                        throw new Error(response.status);
+                    }
+                    
                     if (response.ok) {                   
                         $("#new-word").val(result.word); 
                         addWords();
                     } else {
                         addRandomWords();
-                    }
+                    }return response.json();
+                })
+                .then(function(data){
+                    console.log
                 })
                 .catch(function(error){
                     console.error('Error:', error);
@@ -66,8 +90,16 @@ var addRandomWords = function() {
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
+
+            spinner.hide();
+            $("#generated-word").html("<p>Information not available. Please try again.</p>");
+            $("#generated-word").show();
             addRandomWords();
+        },
+        complete: function() {
+            spinner.hide();
         }
+        
     });
 
 };
