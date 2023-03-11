955e8efaf390ed0f184e2e49a43be50076443a24
pexApred <120075705+pexApred@users.noreply.github.com>
2023-03-11 11:02:59 -0500

    edited randomWord function to sync with dictionaryAPI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // Emmanuel
 // Dictionary API Fetch Function below
-
 var addWords = function() {
     console.log("addWords");
     var word = $("#new-word").val();
@@ -17,35 +16,54 @@ var addWords = function() {
 
             var definition = data[0].meanings[0].definitions[0].definition;
             var phoneticText = data[0].phonetics[0].text;
-            var phoneticAudio = data[0].phonetics[0].audio[1,2,3,4];
+            var phoneticAudio = data[0].phonetics.map(function(phonetics) {
+                return phonetics.audio;
+            });
             console.log(definition);
             console.log(phoneticAudio);
             
-            $("#generated-word").html("<h2>" + word + "</h2><p>" + phoneticText + phoneticAudio + "</p><p>" + definition + "<p>");
+            var audioEl = document.createElement("audio");
+            audioEl.src = phoneticAudio[0];
+            audioEl.controls = true;
 
+            $("#generated-word").html("<h2>" + word + "</h2><p>" + phoneticText + "</p><p>" + definition + "<p>");
+            $("#generated-word").append(audioEl);
         });
 
 }
 // Random Word generator API from API Ninjas
 var addRandomWords = function() {
     console.log("add random words")
-    var randomWord =$("#random-word-search-button");
+    // var randomWord =$("#random-word-search-button");
    
     $.ajax({
         method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/randomword',
+        url: 'https://api.api-ninjas.com/v1/randomword?maxLength=5',
         headers: { 'X-Api-Key': "rbypcBezrmrt/yj6k+bxXg==JjPS9OzXlA7lAUl8"},
         contentType: 'application/json',
         success: function(result) {
             console.log(result);
-            $("#new-word").val(result.word);
+
+            var wordUrl = "https://api.dictionaryapi.dev/api/v2/entries/en_US/" + result.word;
+            fetch(wordUrl)
+                .then(function(response){
+                    if (response.ok) {
+                        $("#new-word").val(result.word); 
+                        addWords();
+                    } else {
+                        addRandomWords();
+                    }
+                })
+                .catch(function(error){
+                    console.error('Error:', error);
+                });
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
         }
     });
 
-}
+};
 
 var addPage = function(){
     console.log("add page");
@@ -60,10 +78,7 @@ var initListeners = function(){
         console.log("submitted form");
 
     });
-
-
     $("#new-word-search-button").click(addWords);
-
     $("#random-word-search-button").click(addRandomWords);
     $("#page").click(addPage);
 }
@@ -73,8 +88,6 @@ $(function() {
     console.log("init");
     initListeners();
     addWords();
-
-
     addPage();
 });
 // Emmanuel
